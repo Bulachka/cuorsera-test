19077f72781d8b7fe99bfea9d802e63ed9d619b1
Bulachka <bella-divina@yandex.by>
2020-11-19 23:32:54 +0300

    all done

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -28,7 +28,7 @@ WARNING!!! WARNING!!!
 */
 
 // STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
+// Wrap the entire contents of script.js inside of an IIFE!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
@@ -55,10 +55,18 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-for (var i = 0; i < names.length; i++) {
-  if (name.charAt(0) === 'J' || name.charAt(0) === 'j') {
-    byeSpeaker.name
+
+
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i]; 
+  if (name.charAt() === 'J' || name.charAt(0) === 'j') {
+    console.log(byeSpeaker.speak(name));
   } else {
-    helloSpeaker.name
+    console.log(helloSpeaker.speak(name));
   }
 }
+
+})();
+
